faa0e2719cdba3d936bab0694c6120eec5039216
Peter Szabó <szabo@expressprint.eu>
2015-02-17 15:20:42 +0200

    Added more cool stuff

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
    
    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+   <link href="css/bootstrap-lightbox.min.css" rel="stylesheet">
+   <link type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/redmond/jquery-ui.css" rel="stylesheet" />
    
    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -16,17 +18,38 @@
     <style type="text/css">
       body {
         padding-top: 80px;
-        padding-bottom: 40px;
+        padding-bottom: 5px;
         background-color: #f5f5f5;
       }
 
+      html, body {
+        height: 100%;
+      }
+      
+      #wrap {
+        min-height: 100%;
+      }
+      
+      #main {
+        overflow:auto;
+        padding-bottom:150px; /* this needs to be bigger than footer height*/
+      }
+      
+      .footer {
+        position: relative;
+        margin-top: -150px; /* negative value of footer height */
+        height: 150px;
+        clear:both;
+        padding-top:20px;
+        padding-left: 40px;
+      } 
     </style>
  </head>
  <body>
-  
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.5.3/modernizr.min.js" type="text/javascript"></script>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
   <script src="js/bootstrap.js"></script>
+  <script src="js/youtube.js"></script>
   <script type="text/javascript">
    function autorun()
    {
@@ -57,10 +80,14 @@
                 m.attr("data-original-title", "Move your cursor over me and catch me!");
                 m.tooltip("show");
               }, 300);
-            }, 1000);
+            }, 1500);
           }, 300);
         }, 800);
       }, 300);
+      
+      $("#ftr").tooltip("show");
+      
+      $("a.youtube").YouTubePopup({autoplay: 1, hideTitleBar: true, width: "800", 'clickOutsideClose': false});
    }
    if (document.addEventListener) document.addEventListener("DOMContentLoaded", autorun, false);
    else if (document.attachEvent) document.attachEvent("onreadystatechange", autorun);
@@ -71,11 +98,27 @@
    }
   </script>
   
-  <div class="container">
+  <div id="wrap">
+  <div id="main" class="container clear-top">
 
       <div style="width: 12em; position: absolute; top:20%; left: 10%">
-        <img id="magic-m" data-toggle="tooltip" data-placement="top" src="img/m.svg" style="width: 100%" />
+        <img id="magic-m" data-toggle="tooltip" data-placement="top" src="http://www.clker.com/cliparts/Z/K/S/d/g/Z/happy-mushroom.svg" style="width: 100%" />
       </div>
     </div> <!-- /container -->
+  </div>
+
+  <?php
+      $db = mysql_connect('localhost:/var/run/mysql/mysql.sock', '-------', '----------');
+      if (!$db) die('connection error '.mysql_error());
+      if (!mysql_select_db('xszabo04', $db)) die('database not available '.mysql_error());
+      
+      $row = mysql_query("SELECT NOW() as date");
+      $data = mysql_fetch_object($row);
+  ?>
+    <footer class="footer">
+      <span id="ftr" data-toggle="tooltip" data-placement="top" title="I've used PHP &amp; MySQL here"><?php echo substr($data->date, 0, 4)." &copy; Magick Mushroom"?></span>
+      |
+      <a class="youtube" href="http://www.youtube.com/watch?v=hGlyFc79BUE">Click here to watch some cool mushroom video!</a>
+    </footer>
  </body>
 </html>
